14ae7ef58a3b65d7acf2da48eaad430f02851f79
Tianyi Hao <haoty0116@gmail.com>
2023-05-24 14:02:17 -0500

    add BPDN variant and Lasso reconstructors

---
+++ oscar/reconstruction/basis_pursuit_denoising_variant_reconstructor.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+import cvxpy as cp
+import numpy as np
+
+from .base_cvxpy_reconstructor import BaseCvxPyReconstructor
+
+
+class BPDNVariantReconstructor(BaseCvxPyReconstructor):
+    def __init__(self, tolerance: float, solver: Optional[str] = None, **solver_kwargs):
+        self.tolerance = tolerance
+        super().__init__(solver, **solver_kwargs)
+
+    def _build_optimization_problem(
+        self, A: np.ndarray, x: cp.Variable, b: np.ndarray
+    ) -> cp.Problem:
+        return cp.Problem(cp.Minimize(cp.norm(x, 1)), [cp.norm(A @ x - b, 2) <= self.tolerance])
