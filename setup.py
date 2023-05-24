a2721483af774832a423d84ca097fa7fbc0d3959
Tianyi Hao <haoty0116@gmail.com>
2023-05-23 22:54:09 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oscar',
+    version='0.1',
+    description='OSCAR: cOmpressed Sensing based Cost lAndscape Reconstruction',
+    author='OSCAR Developers',
+    url='https://github.com/haoty/QSCAR',
+    license='MIT',
+    packages=find_packages(exclude=['test*', 'scripts', 'assets', 'notebooks', 'doc']),
+    python_requires='>=3.8.13',
+    install_requires=[
+        'numpy>=1.24.3',
+        'scipy>=1.10.1',
+        'qiskit>=0.43.0',
+        'qiskit_finance>=0.3.4',
+        'qiskit_optimization>=0.5.0',
+        'qiskit_nature>=0.6.1',
+        'networkx>=2.8.8',
+        'matplotlib>=3.7.1',
+        'cvxpy>=1.3.1'
+    ],
+    # test_suite='test.test',
+)
