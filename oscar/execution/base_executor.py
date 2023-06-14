99f736d4b58a919877f6fb8e6c2ec8911c923642
Tianyi Hao <tianyi.hao@wisc.edu>
2023-06-14 01:22:37 -0400

    add and default to run_batch() in executors for those that can exploit the parallelization

--- oscar/execution/base_executor.py
+++ oscar/execution/base_executor.py
@@ -7,3 +7,6 @@ class BaseExecutor(ABC):
     @abstractmethod
     def run(self, params: np.ndarray) -> float:
         pass
+
+    def run_batch(self, params_list: list[np.ndarray]) -> np.ndarray:
+        return np.array([self.run(params) for params in params_list])
