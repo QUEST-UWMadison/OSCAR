a32e7fed972e314249a6ba9a8479fb8c0975adb1
Tianyi Hao <tianyi.hao@wisc.edu>
2023-06-14 01:28:45 -0400

    add an executor that use the interpolated landscape

---
+++ oscar/execution/interpolated_landscape_executor.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from .base_executor import BaseExecutor
+from ..landscape import Landscape
+
+
+class InterpolatedLandscapeExecutor(BaseExecutor):
+    def __init__(self, landscape: Landscape) -> None:
+        self.landscape = landscape
+        super().__init__()
+
+    def run(self, params: np.ndarray) -> float:
+        return self.landscape.interpolator(params)
+    
