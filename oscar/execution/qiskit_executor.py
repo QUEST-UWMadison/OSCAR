59db6305e7ca7b8e0d6ef0c60fc2b5d5ce2d89c5
Tianyi Hao <tianyi.hao@wisc.edu>
2023-07-11 11:46:40 -0400

    use singledispatch to overload functions

--- oscar/execution/qiskit_executor.py
+++ oscar/execution/qiskit_executor.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
+from functools import singledispatchmethod
 
 from qiskit.algorithms import VQE as OldVQE
 from qiskit.algorithms.minimum_eigensolvers import VQE, SamplingVQE
@@ -11,22 +12,28 @@ from .base_executor import BaseExecutor
 
 
 class QiskitExecutor(BaseExecutor):
-    def __init__(
+    @singledispatchmethod
+    def __init__(self, algorithm: VQE | SamplingVQE, operator: BaseOperator) -> None:
+        self._init(algorithm, operator)
+        if isinstance(algorithm, SamplingVQE):
+            if len(algorithm.ansatz.clbits) > 0:
+                algorithm.ansatz.remove_final_measurements()
+            algorithm.ansatz.measure_all()
+        self.evaluate_energy = algorithm._get_evaluate_energy(
+            ansatz=algorithm.ansatz, operator=operator
+        )
+
+    @__init__.register
+    def _(self, algorithm: OldVQE, operator: OperatorBase) -> None:
+        self._init(algorithm, operator)
+        self.evaluate_energy = algorithm.get_energy_evaluation(operator)
+
+    def _init(
         self, algorithm: OldVQE | VQE | SamplingVQE, operator: OperatorBase | BaseOperator
     ) -> None:
         self.algorithm: OldVQE | VQE | SamplingVQE = algorithm
         self.operator: OperatorBase | BaseOperator = operator
-        algorithm._check_operator_ansatz(operator)
-        if isinstance(algorithm, OldVQE):
-            self.evaluate_energy = algorithm.get_energy_evaluation(operator)
-        else:
-            if isinstance(algorithm, SamplingVQE):
-                if len(algorithm.ansatz.clbits) > 0:
-                    algorithm.ansatz.remove_final_measurements()
-                algorithm.ansatz.measure_all()
-            self.evaluate_energy = algorithm._get_evaluate_energy(
-                ansatz=algorithm.ansatz, operator=operator
-            )
+        algorithm._check_operator_ansatz(operator)  # pylint: disable=protected-access
 
-    def _run(self, params: Sequence[float]) -> float:
-        return self.evaluate_energy(params)
+    def _run(self, params: Sequence[float], *args, **kwargs) -> float:
+        return self.evaluate_energy(params, *args, **kwargs)
