5297b552748927e8c84851835fb77f3891cc83ce
Tianyi Hao <tianyi.hao@wisc.edu>
2024-02-01 22:11:55 -0600

    enhance plot_2d_landscape()

--- oscar/visualization/plot_2d_landscape.py
+++ oscar/visualization/plot_2d_landscape.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Callable, Literal
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.figure import Figure
@@ -11,45 +13,65 @@ from ..optimization import Trace
 def plot_2d_landscape(
     landscape: Landscape,
     trace: Trace | None = None,
-    trace_plot_params: tuple[int, int] = (0, 1),
+    plot_optimum: Literal["min", "max", False, None] | tuple[float, float] = "min",
     show: bool = True,
     figure: Figure | None = None,
+    custom_plot_landscape_func: Callable | None = None,
+    custom_plot_trace_func: Callable | None = None,
+    custom_plot_optimum_func: Callable | None = None,
 ) -> Figure:
     if landscape.num_params != 2:
         raise ValueError("Landscape must be two-dimensional.")
+    landscape = landscape.to_dense()
     fig = plt.figure(figure)
-    plt.imshow(
-        landscape.landscape.T,
-        extent=landscape.param_bounds.flat,
-        origin="lower",
-        interpolation="none",
-    )
-    plt.colorbar()
+    if custom_plot_landscape_func is None:
+        plt.imshow(
+            landscape.landscape.to_numpy().T,
+            extent=landscape.param_bounds.flat,
+            origin="lower",
+            interpolation="none",
+        )
+        plt.colorbar()
+    else:
+        custom_plot_landscape_func(landscape.landscape.to_numpy().T)
     if trace is not None:
-        trace_plot_params = np.array(trace_plot_params)
-        plt.plot(
-            *np.array(trace.params_trace).T[trace_plot_params],
-            alpha=0.75,
-            label="optimizer trace",
-            color="red",
-            linewidth=1,
-        )
-        plt.scatter(
-            *np.array(trace.params_trace).T[trace_plot_params],
-            marker="x",
-            alpha=0.75,
-            label="optimizer query",
-            c=range(len(trace.params_trace)),
-            cmap="autumn",
-            s=15,
-        )
-    plt.scatter(
-        *landscape.optimal_params,
-        marker="*",
-        color="white",
-        s=20,
-        label="grid minima",
-    )
+        params_trace = np.array(trace.params_trace).T
+        if custom_plot_trace_func is None:
+            plt.plot(
+                *params_trace,
+                alpha=0.75,
+                label="optimizer trace",
+                color="red",
+                linewidth=1,
+            )
+            plt.scatter(
+                *params_trace,
+                marker="x",
+                alpha=0.75,
+                label="optimizer query",
+                c=range(len(trace.params_trace)),
+                cmap="autumn",
+                s=15,
+            )
+        else:
+            custom_plot_trace_func(*params_trace)
+    if plot_optimum not in [False, None]:
+        if plot_optimum == "min":
+            optimum = landscape.argmin()
+        elif plot_optimum == "max":
+            optimum = landscape.argmax()
+        else:
+            optimum = plot_optimum
+        if custom_plot_optimum_func is None:
+            plt.scatter(
+                *optimum,
+                marker="*",
+                color="white",
+                s=20,
+                label="grid optimum",
+            )
+        else:
+            custom_plot_optimum_func(optimum)
     plt.legend()
     if show:
         plt.show()
